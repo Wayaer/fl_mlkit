5d0d3ce60c89844b97038941434006d0459468ec
wayaer <wayaer@foxmail.com>
2021-07-24 22:45:44 +0800

    no message

--- ios/Classes/FlMlKitScanningMethodCall.swift
+++ ios/Classes/FlMlKitScanningMethodCall.swift
@@ -1,7 +1,7 @@
 import Flutter
 import Foundation
 
-public class FlMlKitScanningMethodCall: FlCameraMethodCall {
+public class FlMlKitScanningMethodCall: NSObject {
     var registrar: FlutterPluginRegistrar?
 
     init(_ _registrar: FlutterPluginRegistrar) {
@@ -10,4 +10,17 @@ public class FlMlKitScanningMethodCall: FlCameraMethodCall {
     }
 
     public func handle(call: FlutterMethodCall, result: @escaping FlutterResult) {}
+
+//    let buffer = CMSampleBufferGetImageBuffer(sampleBuffer)
+//    let image = VisionImage(image: buffer!.image)
+//    let scanner = BarcodeScanner.barcodeScanner()
+//    scanner.process(image) { [self] barcodes, error in
+//        if error == nil && barcodes != nil {
+//            for barcode in barcodes! {
+//                let event: [String: Any?] = ["name": "barcode", "data": barcode.data]
+//                sink?(event)
+//            }
+//        }
+//        analyzing = false
+//    }
 }
